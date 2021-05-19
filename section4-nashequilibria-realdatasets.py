8c0bdda6b373a44f042deaa403890ad33489c731
astoica <ana.andreea.stoica@gmail.com>
2021-05-18 21:44:27 -0400

    Update section4-nashequilibria-realdatasets.py

--- section4-nashequilibria-realdatasets.py
+++ section4-nashequilibria-realdatasets.py
@@ -1,3 +1,7 @@
+''' This code implements the methodology described in Section 4, computing the Nash equilibria of different utility functions 
+and comparing (1) conductance of Spectral Clustering with the equilibrium solutions and (2) average utility of Spectral Clustering as 
+compared to the equilibrium solutions.'''
+
 import networkx as nx
 import csv 
 import numpy as np
@@ -6,6 +10,7 @@ import copy
 import math 
 from sklearn.cluster import SpectralClustering, KMeans
 
+# the fucnction compute_conductance() computes average conductance for a clustering assignment
 def compute_conductance(G, list_of_nodes_G, cluster_assignment, no_of_clusters):
     clconductance = {}
     for i in range(no_of_clusters):
@@ -20,12 +25,14 @@ def compute_conductance(G, list_of_nodes_G, cluster_assignment, no_of_clusters):
     else:
         return 1 - sum(clconductance.values())/len([v for v in clconductance.values() if v > 0])
 
+# the function cut_size() computes the cut size between a subgraph S and a graph G
 def cut_size(G, S, T=None, weight=None):
     edges = nx.edge_boundary(G, S, T, data=weight, default=1)
     if G.is_directed():
         edges = chain(edges, nx.edge_boundary(G, T, S, data=weight, default=1))
     return sum(weight for u, v, weight in edges)
 
+# the function incluster_degree() computes the incluster degree of a node, given a graph and a specified clustering partition (number of edges that are fully within the same cluster as the source node) 
 def incluster_degree(G, list_nodes_G, cluster_assignment, node):
     degu = 0
     for nbr in G.neighbors(node):
@@ -33,6 +40,7 @@ def incluster_degree(G, list_nodes_G, cluster_assignment, node):
             degu += 1
     return degu
 
+# the function utility_node_divided() computes the closeness utility of a node, given a graph and a clustering partition
 def utility_node_divided(G, list_nodes_G, cluster_assignment, no_clusters, node):
     lengths_total = nx.single_source_shortest_path_length(G, node)
     lengths = {}
@@ -51,7 +59,8 @@ def utility_node_divided(G, list_nodes_G, cluster_assignment, no_clusters, node)
         utility[i] = deg_u / lengths[i] 
     return utility
 
-# this utility function is from Price of Pareto Optimality in hedonic games by elkind et al, namely, w_i(C) / |C| - 1 (or w_i(C)/|C|) where w_i(C) is the sum of utility of node i in cluster C
+# the utility function utility_node_elkind() computes the mfu of a node given a graph and a clustering partition
+# the mfu is from Price of Pareto Optimality in hedonic games by elkind et al, namely, w_i(C) / |C| - 1 (or w_i(C)/|C|) where w_i(C) is the sum of utility of node i in cluster C
 def utility_node_elkind(G, list_nodes_G, cluster_assignment, no_clusters, node):
     cluster_assignment_counterfactual = {}
 
@@ -67,11 +76,12 @@ def utility_node_elkind(G, list_nodes_G, cluster_assignment, no_clusters, node):
 '''#APS dataset: 
 filename = 'APS-clusteringgames-utilities-k' + str(k) + '.csv'
 
+# read in the data as a graph
 G_og = nx.read_gexf('Downloads/clustering_plotting/APS/sampled_APS_pacs052030.gexf')
 
+# work with the largest connected compoenent
 gg = sorted(nx.connected_components(G_og),key=len,reverse=True)[0]
 Gc = G_og.subgraph(gg)
-print(nx.info(Gc))
 
 list_nodes=list(Gc.nodes())
 print("read the APS graph")
@@ -88,6 +98,7 @@ e, v = np.linalg.eig(L.todense())
 '''#Facebook dataset: 
 filename = 'Facebook-clusteringgames-utilities-k' + str(k) + '.csv'
 
+# read in the data as a graph
 Gc = nx.read_edgelist('Facebook/facebook_combined.txt')
 
 list_nodes=list(Gc.nodes())
@@ -104,6 +115,7 @@ gender = {}
 egos = ['0', '107','348','414','686','698','1684','1912','3437','3980']
 genderfeatfinder = {}
 
+# find the sensitive feaures (anonymized gender), and place them in a dictionary
 for u in egos: 
     print(u)
     genderfeatfinder[u] = {}
@@ -132,7 +144,7 @@ for u in egos:
         gender[user] = myrow[int(max(gender_ind))+1]
     f.close()
 
-# this code creates a list, sensitive, that encodes the anonymized gender in the data; it is not used in this section
+# create a list, sensitive[], that encodes the anonymized gender in the data; it is not used in this section
 sensitive = []
 for u in list_nodes:
     if (gender[u] == '1'):
@@ -146,8 +158,10 @@ sensitive
 '''#Highschool dataset:
 filename = 'Highschool-clusteringgames-utilities-k' + str(k) + '.csv'
 
+# read in the data as a graph
 G_og = nx.read_edgelist('Downloads/Friendship-network_data_2013.csv')
 
+# get the largest connected component of the graph
 gg = sorted(nx.connected_components(G_og),key=len,reverse=True)[0]
 Gbig = G_og.subgraph(gg)
 Gc = Gbig.copy()
@@ -157,6 +171,7 @@ print("read the Highschool graph")
 #k = 4
 # finding the spectrum of the graph
 
+# find the sensitive features (unanonymized gender) and place it in a dictionary
 gender = {}
 
 filename = 'Downloads/metadata_2013.txt'
@@ -172,6 +187,7 @@ for u in list_init:
     if gender[u] == 'Unknown':
         Gc.remove_node(u)
         
+# find the spectrum of the graph
 list_nodes = list(Gc.nodes())
 A = nx.adjacency_matrix(Gc)
 L = nx.normalized_laplacian_matrix(Gc)
@@ -183,6 +199,7 @@ e, v = np.linalg.eig(L.todense())
 myfile = open(filename,'w')
 writer= csv.writer(myfile, lineterminator="\n")
 
+# set the number of clusters
 k = 2
 print("k: ", k)
 i = [list(e).index(j) for j in sorted(list(e))[1:k+1]]
@@ -214,7 +231,7 @@ for iter in range(no_iterations):
     assert(list_nodes_copy != list_nodes)
     part = [list_nodes_copy[i::k] for i in range(k)]
 
-    # random partition
+    # random initial partition
     y = np.zeros(len(list_nodes)) 
     for i in range(k):
         y[[list_nodes.index(xx) for xx in part[i]]] = i
@@ -231,15 +248,15 @@ for iter in range(no_iterations):
         x = utility_node_divided(Gc, list_nodes, y_copy, k, u)
         y_util[list_nodes.index(u)] = max(x, key=x.get)
 
-    print(list(y_util) == list(y_copy))
+    #print(list(y_util) == list(y_copy))
 
     y = copy.deepcopy(y_copy)
 
     counter =0 
     while list(y_util) != list(y):
         print("number of unmoved nodes: ", len(np.where(y==y_util)[0]))
-        print(y)
-        print(y_util)
+        #print(y)
+        #print(y_util)
         counter += 1
         if counter > 100:
             break
@@ -296,13 +313,11 @@ for iter in range(no_iterations):
     assert(list_nodes_copy != list_nodes)
     part = [list_nodes_copy[i::k] for i in range(k)]
 
-    # random partition
+    # random initial partition
     y = np.zeros(len(list_nodes)) 
     for i in range(k):
         y[[list_nodes.index(xx) for xx in part[i]]] = i
-    print(y)
 
-    y
     y_copy = copy.deepcopy(y)
     y_util = np.zeros(len(Gc.nodes()))
     mycounter = 0 
@@ -313,7 +328,7 @@ for iter in range(no_iterations):
         x = utility_node_elkind(Gc, list_nodes, y_copy, k, u)
         y_util[list_nodes.index(u)] = max(x, key=x.get)
 
-    print(list(y_util) == list(y_copy))
+    #print(list(y_util) == list(y_copy))
     y = copy.deepcopy(y_copy)
 
     counter =0 
@@ -335,22 +350,22 @@ for iter in range(no_iterations):
             else:
                 y_utiltest[list_nodes.index(u)] = y_util[list_nodes.index(u)]
         y_util = y_utiltest.copy()
-        print(list(y_util) == list(y))
+        #print(list(y_util) == list(y))
     for u in Gc.nodes():
         util_all[iter] += utility_node_divided(Gc, list_nodes, y_util, k, u)[y_util[list_nodes.index(u)]]
     util_all[iter] /= len(list_nodes)
-    print([len(np.where(y_util == kk)[0]) for kk in range(k)])
+    #print([len(np.where(y_util == kk)[0]) for kk in range(k)])
     conductance_random[iter] = compute_conductance(Gc,list_nodes,y_utiltest,k)
 conductance_random_avg = np.mean([x for x in conductance_random.values()])
 conductance_random_std = np.std([xx for xx in conductance_random.values()])
 print("Conductance :",conductance_random_avg,conductance_random_std)
 util_all_avg = np.mean([x for x in util_all.values()])
 util_all_std = np.std([xx for xx in util_all.values()])
-print("Elkind1 utility of random clustering: ",util_all_avg, util_all_std)
+print("mfu utility of random clustering: ",util_all_avg, util_all_std)
 writer.writerow(['conductance mfhg'])
 writer.writerow([conductance_random_avg])
 writer.writerow([conductance_random_std])
-writer.writerow(['mfhg utility'])
+writer.writerow(['mfu utility'])
 writer.writerow([util_all_avg])
 writer.writerow([util_all_std])
 myfile.close()
